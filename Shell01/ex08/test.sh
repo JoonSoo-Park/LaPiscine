4f94d88a364308154433e697b364533d0ee2e3d3
JoonSoo Park <59634896+JoonSoo-Park@users.noreply.github.com>
2021-03-24 18:34:51 +0900

    Add files via upload

---
+++ Shell01/ex08/test.sh
@@ -0,0 +1,9 @@
+export FT_NBR1=\\\'\?\"\\\"\'\\
+export FT_NBR2=rcrdmddd
+
+./add_chelou.sh
+
+FT_NBR1=\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"
+FT_NBR2=dcrcmcmooododmrrrmorcmcrmomo
+
+./add_chelou.sh
