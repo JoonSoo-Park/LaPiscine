31e42a01d3c0a0a57250cb863bdc94d4f0eb357a
JoonSoo Park <59634896+JoonSoo-Park@users.noreply.github.com>
2021-03-24 18:35:46 +0900

    Add files via upload

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | sed '/^#/d' | sed -n '2,$p' | awk -F ":" '{ print $1 }' | rev | sort -fnr | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ',' | sed 's/,$/./' | tr -d '\n'
+cat /etc/passwd | sed '/^#/d' | sed -n '2,$p' | awk -F ":" '{ print $1 }' | rev | sort -fnr | sed -n "${FT_LINE1},${FT_LINE2}p" | tr '\n' ',' | sed 's/,$/./' | sed 's/,/, /g' | tr -d '\n'
