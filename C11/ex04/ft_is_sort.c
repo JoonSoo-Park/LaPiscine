923c0ddf9121a8df40947491c1245e1f6ec04aae
Joonsoo Park <joonpark@c10r1s6.42seoul.kr>
2021-04-12 22:50:32 +0900

    commit

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -6,47 +6,44 @@
 /*   By: joonpark <joonpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 21:00:39 by joonpark          #+#    #+#             */
-/*   Updated: 2021/04/12 21:14:20 by joonpark         ###   ########.fr       */
+/*   Updated: 2021/04/12 22:49:57 by joonpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int			func(int a, int b)
+int			ft_ascending(int *tab, int length, int (*f)(int, int))
 {
-	if (a < b)
-		return (-1);
-	else if (a == b)
-		return (0);
-	else
-		return (1);
+	int		idx;
+
+	idx = 0;
+	while (idx < length - 1)
+	{
+		if (f(tab[idx], tab[idx + 1]) > 0)
+			return (0);
+		++idx;
+	}
+	return (1);
+}
+
+int			ft_descending(int *tab, int length, int (*f)(int, int))
+{
+	int		idx;
+
+	idx = 0;
+	while (idx < length - 1)
+	{
+		if (f(tab[idx], tab[idx + 1]) < 0)
+			return (0);
+		++idx;
+	}
+	return (1);
 }
 
 int			ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
-	int		idx;
-	int		t;
-
 	if (length == 1)
 		return (1);
-	if (a < b)
-		t = -1;
-	else if (a == b)
-		t = 0;
+	if (tab[0] <= tab[1])
+		return (ft_ascending(tab, length, f));
 	else
-		t = 1;
-
-}
-
-#include <stdlib.h>
-#include <stdio.h>
-int main()
-{
-	int *arr;
-	arr = malloc(sizeof(int) * 5);
-
-	arr[0] = 1;
-	arr[1] = 2;
-	arr[2] = 3;
-	arr[3] = 4;
-	arr[4] = 5;
-	printf("%d\n", ft_is_sort(arr, 5, &func));
+		return (ft_descending(tab, length, f));
 }
